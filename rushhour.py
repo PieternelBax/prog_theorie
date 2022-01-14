b50fdb909484cde96696296127ea5a055c3c1b4f
DavidAdriaanse <david20794@gmail.com>
2022-01-14 11:44:12 +0100

    added main

--- rushhour.py
+++ rushhour.py
@@ -1,10 +1,13 @@
+import re
 import pandas as pd
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('csvfile', nargs='?', help="Add game csv path here", default="gameboards/Rushhour6x6_1.csv")
-file = parser.parse_args()
-df = pd.read_csv(file.csvfile)
+if __name__ == '__main__':
 
-print(df)
+    # Import CSV file and read to Pandas Dataframe
+    parser = argparse.ArgumentParser()
+    parser.add_argument('csvfile', nargs='?', help="Add game csv path here", default="gameboards/Rushhour6x6_1.csv")
+    file = parser.parse_args()
+    df = pd.read_csv(file.csvfile)
 
+    print(df)
