fca50ca1d385fce54d2b50dbf946718c8672033d
PieternelBax <pieternel.bax@hotmail.com>
2022-01-14 12:06:19 +0100

    board

--- grid.py
+++ grid.py
@@ -1,1 +1,25 @@
-
+# setting up the grid for Rush Hour
+class grid():
+    # load the file (in this case: source_file)
+    def __init__(self, file):
+        # cars
+        self.cars = self.load_cars(file)
+        # board game grid
+        self.board = self.grid_size(file)
+        # the move to the exit
+        self.end_move
+    def grid_size(self, file):
+        #  Returns a 2D grid array with the starting positions of the car objects.
+        if "12x12" in file:
+            size = 12
+            # or is size = 144
+        elif "6x6" in file:
+            size = 6
+            # or is size = 36
+            # add end_move 
+        elif "9x9" in file:
+            size = 9 
+            # or is size = 81
+        else:
+            print("Try new size of board")
+        
