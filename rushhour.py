4e11edf5bb60e9234411e2cb95506c50f37462ad
DavidAdriaanse <david20794@gmail.com>
2022-01-14 11:37:47 +0100

    Command line load csv

--- rushhour.py
+++ rushhour.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import argparse
 
-pd = pd.read_csv('gameboards/Rushhour6x6_1.csv')
+parser = argparse.ArgumentParser()
+parser.add_argument('csvfile', nargs='?', help="Add game csv path here", default="gameboards/Rushhour6x6_1.csv")
+file = parser.parse_args()
+df = pd.read_csv(file.csvfile)
 
-print(pd)
+print(df)
+
