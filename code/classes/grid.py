87be06c779995099a3ea54929496b633366c592f
DavidAdriaanse <david20794@gmail.com>
2022-01-17 13:01:13 +0100

    Grid implimentation

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -9,18 +9,21 @@ class Grid():
     def grid_size(self, file):
         #  Returns a 2D grid array with the starting positions of the car objects.
         if "12x12" in file:
-            size = 12
-            # or is size = 144
-            self.end_move = [12, 7]
+            # size = 12
+            # # or is size = 144
+            # self.end_move = [12, 7]
+            return "12"
         elif "6x6" in file:
-            size = 6
-            # or is size = 36
-            # add end_move 
-            self.end_move = [6, 3]
+            # size = 6
+            # # or is size = 36
+            # # add end_move 
+            # self.end_move = [6, 3]
+            return "6"
         elif "9x9" in file:
-            size = 9 
-            # or is size = 81
-            self.end_move = [9, 5]
+            # size = 9 
+            # # or is size = 81
+            # self.end_move = [9, 5]
+            return "9"
         else:
-            print("Try new size of board")
+            return "Try new size of board"
         
--- rushhour.py
+++ rushhour.py
@@ -1,17 +1,20 @@
 import pandas as pd
 import argparse
-
-def load_vehicles(df):
-    
+from code.classes.grid import Grid
 
 # parser for command line
 if __name__ == '__main__':
 
     # Import CSV file and read to Pandas Dataframe
     parser = argparse.ArgumentParser()
-    parser.add_argument('csvfile', nargs='?', help="Add game csv path here", default="gameboards/Rushhour6x6_1.csv")
+    parser.add_argument('csvfile', nargs='?', help="Add game csv path here", default="data/Rushhour6x6_1.csv")
     file = parser.parse_args()
     df = pd.read_csv(file.csvfile)
+    
+    new_file = str(file)
+    split_name = new_file.split("'")
+    print(split_name[1])
 
-    load_vehicles(df)
+    grid_return = Grid.grid_size(split_name[1])
 
+    print(grid_return)
