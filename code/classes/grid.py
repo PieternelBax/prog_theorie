65085880ea98eba27ef4a41e809b7da85483ff2c
DavidAdriaanse <david20794@gmail.com>
2022-01-17 12:26:18 +0100

    Changed to capital letter

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -1,10 +1,11 @@
 # setting up the grid for Rush Hour
-class grid():
+class Grid():
     # load the file (in this case: source_file)
     def __init__(self, file):
         self.board = self.grid_size(file)
         # the move to the exit
         self.end_move
+
     def grid_size(self, file):
         #  Returns a 2D grid array with the starting positions of the car objects.
         if "12x12" in file:
