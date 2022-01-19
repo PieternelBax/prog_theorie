29adc63e64b3ab200ffa3fc6b5e5d050d29c08cb
DavidAdriaanse <david20794@gmail.com>
2022-01-19 09:39:42 +0100

    changed grid

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -1,4 +1,5 @@
 # setting up the grid for Rush Hour
+from venv import create
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import LogNorm
@@ -32,29 +33,9 @@ class Grid():
             return "Try new size of board"
 
     def create_grid(self, size):
-        # grid = []
-
-        # size = size + 1
-
-        # for row in range(1, size):
-        #     for col in range(1, size):
-        #         grid.append([row, col])
-
-        nx, ny = (size, size)
-        x = np.linspace(1, size, nx)
-        y = np.linspace(1, size, ny)
-        X, Y = np.meshgrid(x , y)
+        empty_board = [['_' for _ in range(size)] for _ in range(size)]
+        return ("\n".join([str(row) for row in empty_board]))
         
-        # x_2, y_2 = np.meshgrid(x, y, indexing = 'ij')
-        min_max = np.min(x), np.max(x), np.min(y), np.max(y) 
-        res = np.add.outer(range(size), range(size))%2 
-
-        plt.imshow(res)
-        plt.xticks([])
-        plt.yticks([])
-        plt.show()
 
 
         
-        return grid
-           
