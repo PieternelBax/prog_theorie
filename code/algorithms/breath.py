d9bdef7369c4559d26cafadf7dce61c3fdc62b6d
DavidAdriaanse <david20794@gmail.com>
2022-01-27 10:25:51 +0100

    changed pseudocode

--- code/algorithms/breath.py
+++ code/algorithms/breath.py
@@ -8,5 +8,5 @@ from turtle import st
 # Create a set to add visited states.
 # Add stating state to queue
 # Take the first item in queue and add it to visited list
-# Create a list of the adjacent states. Add those which are not within the visited list to the rear of the queue.
+# Create a list of the possible states from the current state. Add those which are not within the visited list to the rear of the queue.
 # Repeat untill queue is empty
