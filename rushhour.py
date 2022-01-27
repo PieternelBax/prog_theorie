ba8a747952bf246f99ae520febe17d67d0611804
meliesja <melissasamsin@hotmail.com>
2022-01-27 16:56:28 +0100

    move function added

--- rushhour.py
+++ rushhour.py
@@ -1,31 +1,34 @@
 from code.classes.grid import Grid
 import code.algorithms.random as random_alg
 import code.algorithms.breadth_first as breadth_first
+import pandas as pd
 import argparse
 import re
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('csvfile', nargs='?', help="Add game csv path here", default="data/Rushhour6x6_1.csv")
-    file = parser.parse_args()
+    parser.add_argument('csvfile', default="data/Rushhour6x6_1.csv", help="Add game csv path here")
+    args = parser.parse_args()
 
     # get grid size from file name
-    size = int(re.findall(r'[0-9]+', f"{file.csvfile}")[0])
+    size = int(re.findall(r'[0-9]+', f"{args.csvfile}")[0])
     
     # get results 
-    total_iterations = 200
-    for i in range(total_iterations):
+    total_iterations = 1
+
+    for _ in range(total_iterations):
         # create grid object
         grid_object = Grid(size)
 
         # load cars on grid
-        grid_object.load_vehicles(file.csvfile)
+        grid_object.load_vehicles(args.csvfile)
 
         # show visual of starting grid
-        # grid_object.visualize_grid()
+        grid_object.visualize_grid()
 
-        # run random algorithm
-        random_alg.random_solver(grid_object)
+        # run random algorithm and store results in list
+        # random_alg.random_solver(grid_object)
+    
 
     # run breadth first algorithm
     breadth_first.breadth_first_solver(grid_object)
