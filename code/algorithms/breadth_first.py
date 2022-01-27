dddca3b490065e9e118d1fbd74306709faa78e67
DavidAdriaanse <david20794@gmail.com>
2022-01-27 10:43:42 +0100

    added more pseudo code

--- code/algorithms/breadth_first.py
+++ code/algorithms/breadth_first.py
@@ -3,14 +3,22 @@ import queue
 import copy
 from turtle import st
 
-"""Breath First algorithm."""
-# Create a Queue
-state_queue = []
+"""Breadth First algorithm."""
 
-# Create a set to add visited states.
-visited = set()
+def breadth_first_solver(grid):
 
-# Add stating state to queue
-# Take the first item in queue and add it to visited list
-# Create a list of the possible states from the current state. Add those which are not within the visited list to the rear of the queue.
-# Repeat untill queue is empty
+    # Create a Queue
+    # Add stating state to queue
+    state_queue = [grid]
+
+    # Create a set to add visited states.
+    visited = set()
+
+    # Take the first item in queue and add it to visited list
+    current_state = state_queue.pop()
+    visited.add(current_state)
+
+    # Create a list of the possible states from the current state. Add those which are not within the visited list to the rear of the queue.
+    # TODO: Create a function find possible moves from current state.
+    
+    # Repeat untill queue is empty
