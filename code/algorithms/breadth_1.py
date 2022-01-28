03e63f366a4a5336ff62914b77bb49bbe33f7405
PieternelBax <pieternel.bax@hotmail.com>
2022-01-28 08:47:54 +0100

    breadth first 2

---
+++ code/algorithms/breadth_1.py
@@ -0,0 +1,47 @@
+import queue
+import copy
+
+def breadth_first_search(grid_object):
+    # store moves made
+    moves_made = []
+    # initialize a queue
+    q = queue.Queue()
+    # add a start node to queue
+    q.put(grid_object)
+    while not q.empty():
+        #get the first node
+        node = q.get()
+        # check is the node is the end node
+        if node.won():
+            # print(f"Valid move made -> Vehicle {vehicle} moved {move}")
+            # visualize solved puzzle
+            grid_object.visualize_grid()
+            # total amount of moves made
+            print(f"Total moves made: {len(moves_made)}")
+            # return moves made
+            return moves_made
+        # get all possible moves for the node
+        moves = node._moves
+        vehicles=node._vehicle_ids
+        # for each move
+        for move in moves:
+            # check if move is possible to make
+            for vehicle in vehicles:
+                if node.won():
+                    # print(f"Valid move made -> Vehicle {vehicle} moved {move}")
+                    # visualize solved puzzle
+                    node.visualize_grid()
+                    # total amount of moves made
+                    print(f"Total moves made: {len(moves_made)}")
+                    # return moves made
+                    return moves_made
+                if node.move(move,vehicle) == True and "{vehicle} moved {move}" not in moves_made :
+                    # print(f"Valid move made -> Vehicle {vehicle} moved {move}")
+                    # visualize grid
+                    grid_object.visualize_grid()
+                    # add move to moves made
+                    moves_made.append(f"{vehicle} moved {move}")
+                    # add node to queue
+                    q.put(node)
+    # return moves made
+    return moves_made
