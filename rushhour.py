abca6e7f1640006529305000c7e352b82af59d68
meliesja <melissasamsin@hotmail.com>
2022-02-02 13:41:06 +0100

    comments add

--- rushhour.py
+++ rushhour.py
@@ -3,8 +3,6 @@ import code.algorithms.random as random_alg
 import code.algorithms.breadth_1 as breadth_1
 import code.algorithms.a_star as a_star
 # import code.visualisation.visualisation as visual
-import pandas as pd
-import code.visualisation.visualisation as visual
 import argparse
 import time
 import re
@@ -31,26 +29,27 @@ def main():
 
         #-------------------------------show visual of starting grid-------------------------------#
         # grid_object.visualize_grid()
-        # run random algorithm
-        # data.append([random_alg.random_solver(grid_object)])
-        #random_alg.random_solver(grid_object)
-        # run breadth first
-        # breadth_1.breadth_first_search(grid_object)
-        a_star.a_star(grid_object)
-    # visualising random
-    # visual.outputCsv(workload=args.csvfile, data=data)
-    # visual.scatterPlot(workload=args.csvfile, data=data)
-    # run breadth first algorithm
-    # breadth_first.breadth_first_solver(grid_object)
 
-        # run breadth first
-        #breadth_1.breadth_first_search(grid_object)
+
+        #-----------------------------------run random algorithm-----------------------------------#
+        # random_solution = random_alg.random_solver(grid_object)
+        # add solution to data list
+        # data.append(random_solution)
+
+        #--------------------------------plot random solutions data--------------------------------#
+        # visual.outputCsv(workload=args.csvfile, data=data)
+        # visual.scatterPlot(workload=args.csvfile, data=data)
+
+
+        #-----------------------------------run a star algorithm-----------------------------------#
+        # a_star.a_star(grid_object)
+
         
         #-------------------------------run breadth first algorithm--------------------------------#
-        start = time.time()
-        breadth_1.breadth_first_search(grid_object)
-        end = time.time()
-        print(f"Breath first solver took {end - start} seconds\n")
+        # start = time.time()
+        # breadth_1.breadth_first_search(grid_object)
+        # end = time.time()
+        # print(f"Breath first solver took {end - start} seconds\n")
 
 
 if __name__ == '__main__':
